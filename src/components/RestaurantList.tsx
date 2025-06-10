c18ec923d9c70d80a1521b414d54b133df991198
May <maykhine.ada@gmail.com>
2025-06-10 15:07:27 -0400

    MVP complete

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -20,7 +20,7 @@ export const RestaurantList = ({
   const cardRefs = useRef<(HTMLDivElement | null)[]>([])
 
   useEffect(() => {
-    if (selection?.scroll) {
+    if (selection?.scroll && window.innerWidth >= 768) {
       const cardIndex = dataArr.findIndex((res) => res.name === selection.name)
       const card = cardRefs.current[cardIndex]
       if (card) {
@@ -28,23 +28,6 @@ export const RestaurantList = ({
       }
     }
   }, [selection, dataArr])
-
-  // const isOpenNow = (data: RestaurantType) => {
-  //   const now = new Date()
-  //   const today = now.toLocaleDateString("en-US", { weekday: "short" })
-  //   const curHour = now.getHours()
-  //   const oysterDays = data.hours || []
-  //   for (let i = 0; i < oysterDays?.length; i++) {
-  //     if (
-  //       today.toLowerCase() === oysterDays[i].day.toLowerCase() &&
-  //       curHour >= oysterDays[i].startHr &&
-  //       curHour < oysterDays[i].endHr
-  //     ) {
-  //       return true
-  //     }
-  //   }
-  //   return false
-  // }
 
   return (
     <div
